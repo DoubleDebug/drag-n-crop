f742c5129128c48c6608e8a1b5c31f843dcb70f4
DoubleDebug <duledodicc@gmail.com>
2023-07-04 23:54:21 +0200

    feat: finalized file validation

--- src/lib/components/Dropzone.svelte
+++ src/lib/components/Dropzone.svelte
@@ -1,105 +1,110 @@
 <script lang="ts">
-	import UploadIcon from '$lib/icons/UploadIcon.svelte';
-	import { FirebaseStorageApi } from '$lib/api/firebase-storage';
-	import { Dropzone } from 'flowbite-svelte';
-	import {
-		rawFileUrl,
-		stage,
-		rawStoragePath,
-		uploadPercentage,
-		isImage,
-		reasonInvalid
-	} from '../../stores/state';
-	import { FileApi } from '$lib/api/file';
+  import UploadIcon from '$lib/icons/UploadIcon.svelte';
+  import { FirebaseStorageApi } from '$lib/api/firebase-storage';
+  import { Dropzone } from 'flowbite-svelte';
+  import {
+    rawFileUrl,
+    stage,
+    rawStoragePath,
+    uploadPercentage,
+    isImage,
+    reasonInvalid,
+    reasonUploadFail,
+  } from '../../stores/state';
+  import { FileApi } from '$lib/api/file';
 
-	let isDragOver = false;
-	$: className = isDragOver ? 'bg-gray-100 dark:border-gray-500 dark:bg-gray-600' : '';
+  let isDragOver = false;
+  $: className = isDragOver
+    ? 'bg-gray-100 dark:border-gray-500 dark:bg-gray-600'
+    : '';
 
-	const handleNewFile = (event: any) => {
-		event.preventDefault();
+  const handleNewFile = (event: any) => {
+    event.preventDefault();
 
-		const files = event.target?.files || event.dataTransfer?.files;
-		if (!files || !files.length || files.length === 0) {
-			console.log('TODO: no files selected');
-			stage.set('failed-to-upload');
-			return;
-		}
-		const firstFile = files[0];
+    const files = event.target?.files || event.dataTransfer?.files;
+    if (!files || !files.length || files.length === 0) {
+      console.log('TODO: no files selected');
+      stage.set('failed-to-upload');
+      return;
+    }
+    const firstFile = files[0];
 
-		// validation
-		const validation = FileApi.isFileValid(firstFile);
-		if (!validation.isValid) {
-			stage.set('failed-to-upload');
-			reasonInvalid.set(validation.reason);
-			return;
-		}
-		isImage.set(validation.isImage);
+    // validation
+    const validation = FileApi.isFileValid(firstFile);
+    if (!validation.isValid) {
+      stage.set('failed-to-upload');
+      reasonInvalid.set(validation.reason);
+      return;
+    }
+    isImage.set(validation.isImage);
 
-		stage.set('uploading');
-		const path = FirebaseStorageApi.uploadFile({
-			file: firstFile,
-			isImage: true,
-			onStateChange: (snapshot) => {
-				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-				const progressFormatted = Math.round(progress).toString();
-				uploadPercentage.set(progressFormatted);
-			},
-			onSuccess: (url) => {
-				rawFileUrl.set(url);
-				stage.set('ready-to-crop');
-			},
-			onError: () => {
-				stage.set('failed-to-upload');
-			}
-		});
-		rawStoragePath.set(path);
-	};
+    stage.set('uploading');
+    const path = FirebaseStorageApi.uploadFile({
+      file: firstFile,
+      isImage: $isImage,
+      onStateChange: (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progressFormatted = Math.round(progress).toString();
+        uploadPercentage.set(progressFormatted);
+      },
+      onSuccess: (url) => {
+        rawFileUrl.set(url);
+        stage.set('ready-to-crop');
+      },
+      onError: (error) => {
+        reasonUploadFail.set('Error code: ' + error.code);
+        stage.set('failed-to-upload');
+      },
+    });
+    rawStoragePath.set(path);
+  };
 </script>
 
 <Dropzone
-	on:drop={(e) => e.preventDefault()}
-	on:change={handleNewFile}
-	on:drop={handleNewFile}
-	on:dragover={(e) => {
-		e.preventDefault();
-		isDragOver = true;
-	}}
-	on:dragleave={(e) => {
-		e.preventDefault();
-		isDragOver = false;
-	}}
-	class={`dropzone ${className}`}
+  on:drop={(e) => e.preventDefault()}
+  on:change={handleNewFile}
+  on:drop={handleNewFile}
+  on:dragover={(e) => {
+    e.preventDefault();
+    isDragOver = true;
+  }}
+  on:dragleave={(e) => {
+    e.preventDefault();
+    isDragOver = false;
+  }}
+  class={`dropzone ${className}`}
 >
-	<div class="flex justify-center align-center flex-col h-full">
-		<div class="text-5xl mb-5">
-			<UploadIcon />
-		</div>
-		<p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-			<span class="font-semibold">Click to upload</span> or drag and drop
-		</p>
-		<!-- <p class="text-xs text-gray-500 dark:text-gray-400 mb-1">
+  <div class="flex justify-center align-center flex-col h-full">
+    <div class="text-5xl mb-5">
+      <UploadIcon />
+    </div>
+    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+      <span class="font-semibold">Click to upload</span> or drag and drop
+    </p>
+    <!-- <p class="text-xs text-gray-500 dark:text-gray-400 mb-1">
       Supported image formats: jpg, jpeg, png, gif, bmp, webp, svg (MAX 1GB).
     </p>
     <p class="text-xs text-gray-500 dark:text-gray-400">
       Supported video formats: mp4, mkv, flv, avi, mov, wmv, webm (MAX 100MB).
     </p> -->
-	</div>
+  </div>
 </Dropzone>
 
 <style>
-	:global(.dropzone) {
-		height: 100% !important;
-		min-height: 320px;
-	}
+  :global(.dropzone) {
+    height: 100% !important;
+    min-height: 320px;
+  }
 
-	@media only screen and (max-width: 768px) {
-		:global(.dropzone) {
-			justify-self: center;
-		}
-	}
+  @media only screen and (max-width: 768px) {
+    :global(.dropzone) {
+      justify-self: center;
+    }
+  }
 
-	:global(.dropzone) > :global(label) {
-		width: 100%;
-		height: 100%;
-	}
+  :global(.dropzone) > :global(label) {
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/lib/components/FailedToUpload.svelte
+++ src/lib/components/FailedToUpload.svelte
@@ -1,9 +1,89 @@
 <script>
-	import { Button } from 'flowbite-svelte';
-	import { reasonInvalid, stage } from '../../stores/state';
+  import {
+    Accordion,
+    AccordionItem,
+    Badge,
+    Button,
+    Card,
+    Li,
+    List,
+  } from 'flowbite-svelte';
+  import { reasonInvalid, reasonUploadFail, stage } from '../../stores/state';
+  import { FileApi } from '$lib/api/file';
+  import InfoIcon from '$lib/icons/InfoIcon.svelte';
+
+  let imageExtensions = FileApi.validImageFormats;
+  let videoExtensions = FileApi.validVideoFormats;
+  $: invalidMessage = (() => {
+    switch ($reasonInvalid) {
+      case 'invalid-file-format':
+        return `The format of the selected file isn't supported.`;
+      case 'image-too-large':
+        return `The selected image was too large. The size limit is ${
+          FileApi.imageSizeLimit / 1024 / 1024
+        } MB.`;
+      case 'video-too-large':
+        return `The selected image was too large. The size limit is ${
+          FileApi.videoSizeLimit / 1024 / 1024
+        } MB.`;
+      default:
+        return `There was an error while uploading your file. Please try again.`;
+    }
+  })();
 </script>
 
-<div class="grid gap-2">
-	<p>{$reasonInvalid}</p>
-	<Button on:click={() => stage.set('ready-to-upload')}>Try again</Button>
+<div class="grid gap-2 w-full">
+  <Card class="flex max-w-none">
+    <div class="grid gap-3">
+      <Accordion>
+        {#if $reasonInvalid}
+          <AccordionItem open>
+            <span slot="header">
+              <div class="flex items-center gap-2 text-red-400">
+                <InfoIcon />
+                {invalidMessage}
+              </div></span
+            >
+            <List list="disc">
+              <Li liClass="mb-1 text-left whitespace-nowrap">
+                Supported image formats:
+                {#each imageExtensions as extension}
+                  <div class="inline-block ml-2">
+                    <Badge color="dark">{extension}</Badge>
+                  </div>
+                {/each}
+              </Li>
+              <Li liClass="mb-1 text-left whitespace-nowrap">
+                Supported video formats:
+                {#each videoExtensions as extension}
+                  <div class="inline-block ml-2">
+                    <Badge color="dark">{extension}</Badge>
+                  </div>
+                {/each}
+              </Li>
+              <Li liClass="mb-1 text-left whitespace-nowrap">
+                Maximum image size: {FileApi.imageSizeLimit / 1024 / 1024} MB.
+              </Li>
+              <Li liClass="mb-1 text-left whitespace-nowrap">
+                Maximum video size: {FileApi.videoSizeLimit / 1024 / 1024} MB.
+              </Li>
+            </List>
+          </AccordionItem>
+        {:else}
+          <AccordionItem open>
+            <span slot="header">
+              <div class="flex items-center gap-2 text-red-400">
+                <InfoIcon />
+                There was an error while uploading the file.
+              </div>
+            </span>
+            <p class="text-left">{$reasonUploadFail}</p>
+          </AccordionItem>
+        {/if}
+      </Accordion>
+      <Button class="lg:w-40" on:click={() => stage.set('ready-to-upload')}>
+        Try again
+      </Button>
+    </div>
+  </Card>
 </div>
